65aebeeeae50ab4b1cc56e0fc4dd4360b35e392e
occworx <occ@gmx.at>
2022-11-18 17:22:01 +0100

    chng

--- src/app.js
+++ src/app.js
@@ -5,12 +5,12 @@ const app = express();
 const { debug } = require('console');
 
 
-app.set('views', path.join( __dirname + 'views'));
+app.set('views', path.join( __dirname + '/views'));
 app.set('view engine','ejs');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
-app.get('/',(req,res) => {
+app.get('/', (req,res) => {
     res.render('index', { title: 'Index' });
 });
 
