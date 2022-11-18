03021821659088bcfd88845d5f197243100bf28c
occworx <occ@gmx.at>
2022-11-18 17:56:24 +0100

    Completed module

--- src/app.js
+++ src/app.js
@@ -9,17 +9,35 @@ app.set('views', path.join( __dirname + '/views'));
 app.set('view engine','ejs');
 
 app.use(express.static(path.join(__dirname, '/public')));
-
-const accountData = fs.readFileSync('src/json/accounts.json', { encoding : 'UTF8' });
-
+console.log(path.join(__dirname,'/json','/accounts.json'));
+const accountData = fs.readFileSync(
+    path.join(__dirname,'/json','/accounts.json'), { encoding : 'UTF8' }
+);
 const accounts = JSON.parse(accountData);
 
-const userData  = fs.readFileSync('src/json/users.json', { encoding : 'UTF8' });
-
+const userData  = fs.readFileSync(
+    path.join(__dirname,'/json','/users.json'), { encoding : 'UTF8' }
+);
 const users = JSON.parse(userData);
 
 app.get('/', (req,res) => {
     res.render('index', { title: 'Account Summary', accounts: accounts });
+});
+
+app.get('/savings', (req,res) => {
+  res.render('account',{ account: accounts.savings });
+});
+
+app.get('/checkings', (req,res) => {
+    res.render('account',{ account: accounts.checkings });
+  });
+
+app.get('/credit', (req,res) => {
+    res.render('account',{ account: accounts.credit });
+});
+
+app.get('/profile', (req,res) => {
+    res.render('profile', { user: user[0] });
 });
 
 app.listen(3000, () => {
