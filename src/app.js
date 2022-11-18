e56b0845a0f7ee0ea1286b4d5a4a5c20b4ce998f
occworx <occ@gmx.at>
2022-11-18 17:13:34 +0100

    corr

--- src/app.js
+++ src/app.js
@@ -5,10 +5,10 @@ const app = express();
 const { debug } = require('console');
 
 
-app.set('views', path.join( __dirname + '/views'));
+app.set('views', path.join( __dirname + 'views'));
 app.set('view engine','ejs');
 
-app.use(express.static(path.join(__dirname, '/public/')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/',(req,res) => {
     res.render('index', { title: 'Index' });
